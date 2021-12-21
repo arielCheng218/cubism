5a51387d8b148b6e05c33856c27da28f80d0dcd4
Ariel <Cheng>
2021-12-21 09:34:22 +0800

    download tensorflow, data processing

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 
-print("Hello world!")
+import os
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+# Constants
+IMAGE_SIZE = 128
+IMAGE_CHANNELS = 3
+IMAGE_DIR = 'dataset/'
+
+images_path = IMAGE_DIR 
+
+training_data = []
+
+# Iterating over the images inside the directory and resizing them using
+# Pillow's resize method.
+print('resizing images...')
+
+for filename in os.listdir(images_path):
+    path = os.path.join(images_path, filename)
+    image = Image.open(path).resize((IMAGE_SIZE, IMAGE_SIZE), Image.ANTIALIAS)
+
+    training_data.append(np.asarray(image))
+
+training_data = np.reshape(
+    training_data, (-1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS))
+training_data = training_data / 127.5 - 1
+
+print('saving file...')
+np.save('cubism_data.npy', training_data)
